4b4e21ccb2875f6a064fba008c0ce163c0b158fb
Madhukar Jha <madhukar.jha@gmail.com>
2021-02-25 09:42:03 +0530

    custom micro frontend config fixes

--- custom-microfrontend/container/src/App.js
+++ custom-microfrontend/container/src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  NavLink,
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import "./App.css";
+import MicroFrontend from "./MicroFrontend";
 
+const {
+  REACT_APP_CONTENT_APP1: subapp1,
+  REACT_APP_CONTENT_APP2: subapp2,
+} = process.env;
+
+const App1 = ({ history }) => (
+  <MicroFrontend history={history} host={subapp1} name="subapp1" />
+);
+const App2 = ({ history }) => (
+  <MicroFrontend history={history} host={subapp2} name="subapp2" />
+);
+const Home = () => (
+  <>
+    <p>Rendered by Container</p>
+  </>
+);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div style={{ padding: 25, display: "flex" }}>
+        <div style={{ padding: "0px 15px" }}>
+          <NavLink
+            style={{
+              textDecoration: "none",
+              fontWeight: "bold",
+              color: "#282c34",
+              fontSize: 20,
+            }}
+            to="/home"
+          >
+            Home
+          </NavLink>
+        </div>
+        <div style={{ padding: "0px 15px" }}>
+          <NavLink
+            style={{
+              textDecoration: "none",
+              fontWeight: "bold",
+              color: "#282c34",
+              fontSize: 20,
+            }}
+            to="/subapp1"
+          >
+            SubApp1
+          </NavLink>
+        </div>
+        <div style={{ padding: "0px 15px" }}>
+          <NavLink
+            style={{
+              textDecoration: "none",
+              fontWeight: "bold",
+              color: "#282c34",
+              fontSize: 20,
+            }}
+            to="/subapp2"
+          >
+            SubApp2
+          </NavLink>
+        </div>
+      </div>
+
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Route path="/subapp1" render={() => <App1 />} />
+        <Route path="/subapp2" render={() => <App2 />} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ custom-microfrontend/sub-app2/src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App">App2</div>;
+}
+
+export default App;
